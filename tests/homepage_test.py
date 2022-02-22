6173e0c4fd46e4b1286c7bc5b26fa942ffc9bd5d
kaw393939 <keith@webizly.com>
2022-02-22 13:02:16 -0500

    changed homepage for a test

--- tests/homepage_test.py
+++ tests/homepage_test.py
@@ -4,4 +4,4 @@
 def test_request_example(client):
     """This makes the index page"""
     response = client.get("/")
-    assert b"Hello, World!" in response.data
+    assert b"Hello, Class!" in response.data
