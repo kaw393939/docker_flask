d9b5dc235ae4e0c231a268dc073566f56a51a66c
kaw393939 <keith@webizly.com>
2022-02-23 18:14:14 -0500

    updated Simple Pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 """A simple flask web app"""
 from flask import Flask, render_template
+from app.simple_pages import simple_pages
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-    @app.route("/")
-    def index():
-        return render_template('index.html')
+    app.register_blueprint(simple_pages)
 
     return app
---
+++ app/simple_pages/__init__.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+simple_pages = Blueprint('simple_page', __name__,
+                        template_folder='templates',static_folder='static')
+
+
+@simple_pages.route('/', defaults={'page': 'index'})
+@simple_pages.route('/<page>')
+def show(page):
+    try:
+        return render_template('%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
