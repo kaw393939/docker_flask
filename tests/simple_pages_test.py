1582f244855071c695432dc7876198184340781e
kaw393939 <keith@webizly.com>
2022-02-24 14:09:37 -0500

    Added page 3

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -24,3 +24,14 @@ def test_request_page2(client):
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Page 2" in response.data
+
+def test_request_page2(client):
+    """This makes the index page"""
+    response = client.get("/page2")
+    assert response.status_code == 200
+
+def test_request_page3(client):
+    """This makes the index page"""
+    response = client.get("/page3")
+    assert response.status_code == 200
+    assert b"Page 3" in response.data
