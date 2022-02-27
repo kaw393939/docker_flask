8f51799a04cf0dba1ce6468b213d64bdb6dd0a3d
kaw393939 <keith@webizly.com>
2022-02-27 18:40:21 -0500

    database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,30 @@
 """A simple flask web app"""
-from flask import Flask, render_template
+import os
+from flask import Flask
 from app.context_processors import utility_text_processors
 from app.simple_pages import simple_pages
-
-
+from app.db.models import db
+from app.db import database
+from app.auth import auth
+from app.cli import create_database
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
     app.register_blueprint(simple_pages)
+    app.register_blueprint(auth)
     app.context_processor(utility_text_processors)
+    db_dir = "database/db.sqlite"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    # add command function to cli commands
+    app.cli.add_command(create_database)
 
     return app
+
+
+
+
+
+
