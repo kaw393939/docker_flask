4836abc689b6d7c8191cc64196120ba2a62d36e1
kaw393939 <keith@webizly.com>
2022-03-09 11:56:11 -0500

    Added Calculation History

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,8 +1,10 @@
 """ This is the Calculator Class"""
 from calculator.calculations import Addition, Subtraction, Multiplication
 
+
 class Calculator:
     """ This is the default result property"""
+
     @staticmethod
     def add(tuple_list):
         """ This is the add method"""
@@ -15,9 +17,9 @@ class Calculator:
         """ This is the subtract method"""
         calculation = Subtraction.create(tuple_list)
         return calculation.get_result()
+
     @staticmethod
     def multiply(tuple_list):
         """ This is the subtract method"""
         calculation = Multiplication.create(tuple_list)
         return calculation.get_result()
-
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,20 +3,27 @@
 from calculator import Calculator
 
 
+def tuple_list():
+    """Arranging Data for AAA Testing"""
+    return 1.0, 2
+
+
 def test_calculator_add_method():
     """Testing the Calculator"""
     # this is show using the calculator object add method
-    tuple_list = (1.0, 2)
-    assert Calculator.add(tuple_list) == 3
+
+    ## Act for AAA testing
+    result = Calculator.add(tuple_list())
+
+    ## Assertion for AAA testing
+    assert result == 3
 
 
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
-    tuple_list = (1, 2)
-    assert Calculator.subtract(tuple_list) == -3
+    assert Calculator.subtract(tuple_list()) == -3
 
 
 def test_calculator_multiply_method():
     """Testing the Calculator Subtract"""
-    tuple_list = (1, 2)
-    assert Calculator.multiply(tuple_list) == 2
+    assert Calculator.multiply(tuple_list()) == 2
